7f944e6e5fd00e94e9e702732f53f7f3ac9c9ca5
Ivan Mostovyi <ivan.mostovyi.work@gmail.com>
2022-06-11 17:56:28 +0300

    Added swagger

--- big-board-api/src/main/java/com/university/bigboardorganization/bigboardapi/config/WebConfig.java
+++ big-board-api/src/main/java/com/university/bigboardorganization/bigboardapi/config/WebConfig.java
@@ -10,15 +10,30 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
 
+@EnableSwagger2
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.university.bigboardorganization.bigboardapi"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
     @Override

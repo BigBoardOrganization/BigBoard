951a3b93eadaabe2d181b6873a4a782708a0b895
Ivan Mostovyi <ivan.mostovyi.work@gmail.com>
2022-06-11 18:25:56 +0300

    Fixed CORS

--- big-board-api/src/main/java/com/university/bigboardorganization/bigboardapi/config/WebConfig.java
+++ big-board-api/src/main/java/com/university/bigboardorganization/bigboardapi/config/WebConfig.java
@@ -54,7 +54,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("**")
+                .allowedOrigins("http://localhost:4200")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);

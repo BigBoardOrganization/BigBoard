b4455b17f5cfb9bd2a9ba11bb446a323011c96d4
Ivan Mostovyi <ivan.mostovyi.work@gmail.com>
2022-06-19 23:09:07 +0300

    Try to fix reload page while user is on the post

--- big-board-api/src/main/java/com/university/bigboardorganization/bigboardapi/config/WebConfig.java
+++ big-board-api/src/main/java/com/university/bigboardorganization/bigboardapi/config/WebConfig.java
@@ -38,7 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/*")
+        registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver() {
